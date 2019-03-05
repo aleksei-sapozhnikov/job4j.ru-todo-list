e9a8d05ae8d5b87ac8b124316f867c270998982e
vermucht <vermucht@gmail.com>
2019-03-05 15:51:28 +0300

    Added hibernate and db-storage class (empty)

---
+++ src/main/java/todolist/persistence/DbStorage.java
@@ -0,0 +1,18 @@
+package todolist.persistence;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Database storage api.
+ *
+ * @author Aleksei Sapozhnikov (vermucht@gmail.com)
+ * @version 0.1
+ * @since 0.1
+ */
+public class DbStorage {
+    /**
+     * Logger.
+     */
+    private static final Logger LOG = LogManager.getLogger(DbStorage.class);
+}
