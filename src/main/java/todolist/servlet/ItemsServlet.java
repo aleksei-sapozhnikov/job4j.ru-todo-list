ba62bb581cc77d12e8b5a91b075b957609de5264
vermucht <vermucht@gmail.com>
2019-03-07 04:12:30 +0300

    Added hibernate (postgresql), liquibase, marking items done

--- src/main/java/todolist/servlet/ItemsServlet.java
+++ src/main/java/todolist/servlet/ItemsServlet.java
@@ -9,15 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-<<<<<<<HEAD
-=======
-        >>>>>>>development
-        <<<<<<<HEAD
-=======
-        >>>>>>>development
 
 /**
  * Servlet working with items.
@@ -27,34 +18,18 @@ import java.util.List;
  * @since 0.1
  */
 public class ItemsServlet extends HttpServlet {
-<<<<<<<HEAD
-    private final List<Item> storage = new ArrayList<>();
-=======
     private static final int RESP_CODE_CREATED = 201;
     private final ItemDatabaseStorage storage = new ItemDatabaseStorage();
->>>>>>>development
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try (var writer = resp.getWriter()) {
-<<<<<<<HEAD
-            new Gson().toJson(this.storage, writer);
-=======
             new Gson().toJson(this.storage.getAll(), writer);
->>>>>>>development
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-<<<<<<<HEAD
-        Item item;
-        try (var reader = req.getReader()) {
-            item = new Gson().fromJson(reader, Item.class);
-        }
-        this.storage.add(item);
-        resp.setStatus(201);
-=======
         try (var reader = req.getReader();
              var writer = resp.getWriter()
         ) {
@@ -64,6 +39,5 @@ public class ItemsServlet extends HttpServlet {
             gson.toJson(item, writer);
         }
         resp.setStatus(RESP_CODE_CREATED);
->>>>>>>development
     }
 }
